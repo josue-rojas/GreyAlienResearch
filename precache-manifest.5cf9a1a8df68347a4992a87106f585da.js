2912dec0fae26fa71739bf68a2ab3d3374cc7ce5
josue rojas <josuerojas.rojas@gmail.com>
2019-03-17 17:37:48 -0400

    Updates

---
+++ precache-manifest.5cf9a1a8df68347a4992a87106f585da.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "25f3a1b1af9b74ef64867e0f2d0031fe",
+    "url": "/SignupSite/static/media/death-star.25f3a1b1.svg"
+  },
+  {
+    "revision": "24537b6d608d74d92695",
+    "url": "/SignupSite/static/css/main.f04c6eb6.chunk.css"
+  },
+  {
+    "revision": "1875356badde6474c2e4",
+    "url": "/SignupSite/static/js/runtime~main.1875356b.js"
+  },
+  {
+    "revision": "928e8f090de7ca0b2637",
+    "url": "/SignupSite/static/js/2.928e8f09.chunk.js"
+  },
+  {
+    "revision": "29b5a0a90c41371f2d6e11e7a7c45834",
+    "url": "/SignupSite/static/media/alien-with-aqualung.29b5a0a9.svg"
+  },
+  {
+    "revision": "24537b6d608d74d92695",
+    "url": "/SignupSite/static/js/main.24537b6d.chunk.js"
+  },
+  {
+    "revision": "3ae23cc14ae61f8b5c83fc22c4a49802",
+    "url": "/SignupSite/static/media/extraterrestial-head.3ae23cc1.svg"
+  },
+  {
+    "revision": "45f2210edc108b3eb785366076f63693",
+    "url": "/SignupSite/static/media/ufo-and-cow.45f2210e.svg"
+  },
+  {
+    "revision": "fc6bf0dd33452bbc64428ef5225cb329",
+    "url": "/SignupSite/static/media/solar-system.fc6bf0dd.svg"
+  },
+  {
+    "revision": "034201a8c01b91e7a0d75c05cc3407e3",
+    "url": "/SignupSite/static/media/big-ufo.034201a8.svg"
+  },
+  {
+    "revision": "096e4889d89114901617f74f5c835515",
+    "url": "/SignupSite/index.html"
+  }
+];
