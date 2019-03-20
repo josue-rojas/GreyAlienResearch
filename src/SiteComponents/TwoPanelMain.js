40d0d301604dc9ec6803b3bedf7234679e14335d
josue rojas <josuerojas.rojas@gmail.com>
2019-03-20 19:48:55 -0400

    minor changes

--- src/SiteComponents/TwoPanelMain.js
+++ src/SiteComponents/TwoPanelMain.js
@@ -5,13 +5,37 @@ import SlideShowPanel from './SlideShowPanel';
 import SigninPanel from './SigninPanel';
 import Loader from '../Components/Loader';
 import firebase from '../firebase';
+import Button from '../Components/Buttons';
 
+// TODO: rename screen to panel
 // handles loading, main page, and slideshow/sign in page all withing the two-panel component
 function LoadingScreen(){
   // umm. resusing the slidehow classes (too lazy to style again for this)
-  return(
+  return (
     <div className='slideshow-panel panel-content' style={{position: 'relative'}}>
       <Loader isLoading={true} inverse={true}/>
+    </div>
+  )
+}
+
+function signout(){
+  firebase.auth().signOut().then(function() {
+  // Sign-out successful.
+  console.log('succeess')
+}).catch(function(error) {
+  // An error happened.
+  console.log('error signout')
+});
+}
+
+function UserScreen(props){
+  return (
+    <div className='slideshow-panel panel-content'>
+    User
+    <Button
+      onClick={signout}
+      text='Sign Out'
+    />
     </div>
   )
 }
@@ -37,7 +61,6 @@ class TwoPanelMain extends React.Component {
         isSignin: user !== null,
         initStart: false
       })
-      console.log('pathname', thisWrapper.props.location.pathname);
       if(user === null && thisWrapper.props.location.pathname === '/'){
         thisWrapper.props.history.push('/signin');
       }
@@ -57,7 +80,7 @@ class TwoPanelMain extends React.Component {
       return (<LoadingScreen/>)
     }
     else if(this.props.location.pathname === '/') {
-      return (<div className='slideshow-panel panel-content'>Not loading...</div>)
+      return (<UserScreen/>)
     }
     else return <SlideShowPanel/>
   }
